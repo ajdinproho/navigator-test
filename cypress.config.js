3545c6cc3e98f022dcbb5078f6ce8b2a0cbabbef
ajdinproho <ajdinproho@gmail.com>
2025-04-29 00:09:37 +0200

    update Cypress configuration to set test isolation and viewport dimensions

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    testIsolation: false,
+    viewportHeight: 982,
+    viewportWidth: 1512,
   },
 });
