0fd25985c09c8aa24b719af960cb9565d0a81634
ajdinproho <ajdinproho@gmail.com>
2025-04-29 00:12:32 +0200

    add getCategory method and add test case Provjeri header i jesu li default kategorije prikazane

--- cypress/e2e/visitNavigatorPage.cy.js
+++ cypress/e2e/visitNavigatorPage.cy.js
@@ -4,4 +4,22 @@ describe('Navigator test', () => {
     //check url
     cy.url().should('include', 'www.navigator.ba/#/categories')
   });
+  it('Provjeri header i jesu li default kategorije prikazane', () => {
+    cy.get('#header_container').find('.navigation').find('span').should('have.class', 'iconav-plus');
+    cy.get('#header_container').find('.navigation').find('.iconav-plus').parent().find('.text').contains('Kreiraj objekat').should('be.visible');
+    cy.get('#header_container').find('.navigation').find('span').should('have.class', 'iconav-bubble-2');
+    cy.get('#header_container').find('.navigation').find('.iconav-bubble-2').parent().find('.text').contains('Predloži ideju - Pošalji komentar').should('be.visible');
+    cy.getCategory('SMJEŠTAJ');
+    cy.getCategory('KAFA');
+    cy.getCategory('HRANA');
+    cy.getCategory('NOĆNI ŽIVOT');
+    cy.getCategory('KUPOVINA');
+    cy.getCategory('ATRAKCIJE');
+    cy.getCategory('UMJETNOST I ZABAVA');
+    cy.getCategory('SPORT');
+    cy.getCategory('PRIJEVOZ');
+    cy.getCategory('USLUGE');
+    cy.getCategory('BIZNIS');
+    cy.getCategory('GRADSKE ULICE');
+  });
 });
